8451eca2c4402d2817d164ddd3da630c6dd5a7de
sanaryan175 <sanskritig1705@gmail.com>
2025-07-30 16:08:32 +0530

    some change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function submitForm(event) {
   event.preventDefault(); // Stop default behavior
-  // Do validation or store logic here if needed
+  
   window.location.href = "https://sanaryan175.github.io/LingoQuest/thankyou.html";
 // Instant redirect
 }
