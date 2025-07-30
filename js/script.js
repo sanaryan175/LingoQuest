07e9be6d8a37e28f0eb3ed9e9aff0e111ab5e534
sanaryan175 <sanskritig1705@gmail.com>
2025-07-30 17:56:35 +0530

    added navbar toggling feature

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ function submitForm(event) {
   window.location.href = "https://sanaryan175.github.io/LingoQuest/thankyou.html";
 // Instant redirect
 }
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
